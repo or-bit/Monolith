f6aa358017a95dded420e8fc43b9ac61e20e37d1
Nicola <nicola.dalmaso@outlook.com>
2017-05-18 16:39:53 +0200

    Restore BubbleMemory as of specification

--- packages/monolith-api-model/src/Bubble/BubbleMemory/BubbleMemory.js
+++ packages/monolith-api-model/src/Bubble/BubbleMemory/BubbleMemory.js
@@ -3,27 +3,6 @@ const EventEmitter = require('events');
 /**
  * @class BubbleMemory - TODO
  */
-class BubbleMemory extends EventEmitter {
-    constructor(store) {
-        super();
-        console.log(store);
-        // this.memory = createStore(reducers, state);
-    }
-
-    subscribe(onChangeStateFunction) {
-        if (typeof onChangeStateFunction === 'function') {
-            this.memory.subscribe(onChangeStateFunction);
-        } else {
-            throw new Error('Function parameter expected.');
-        }
-    }
-
-    unsubscribe(onChangeStateFunction) {
-        // FAKE LINES ADDED FOR ESLINT UNTIL CLASS IS DONE
-        console.log(this);
-        onChangeStateFunction();
-    }
-
-}
+class BubbleMemory extends EventEmitter {}
 
 module.exports = BubbleMemory;
