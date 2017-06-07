ebeacf0c7d18adee6c86490fd76f362bd59beb49
Nicola <nicola.dalmaso@outlook.com>
2017-06-07 18:02:12 +0200

    IDGenerator exported to public API

--- packages/monolith-backend/index.js
+++ packages/monolith-backend/index.js
@@ -1,17 +1,19 @@
-const BubbleMemory = require('./src/Bubble/BubbleMemory/BubbleMemory');
 const Action = require('./src/Model/Actions/Action');
+const BubbleMemory = require('./src/Bubble/BubbleMemory/BubbleMemory');
 const DataBase = require('./src/Model/DataBase/DataBase');
-const REST = require('./src/Model/ExternalAPI/RESTAPI');
+const IDGenerator = require('./src/IDGenerator/IdGenerator');
 const LifeCycle = require('./src/Model/LifeCycle/LifeCycle');
 const RegularExp = require('./src/Model/RegularExpr/RegularExprWrapper');
-const User = require('./src/Model/User/User');
+const REST = require('./src/Model/ExternalAPI/RESTAPI');
 const Server = require('./src/Server/Server');
 const StoreProvider = require('./src/Bubble/BubbleMemory/StoreProvider');
+const User = require('./src/Model/User/User');
 
 module.exports = {
     Action,
     BubbleMemory,
     DataBase,
+    IDGenerator,
     LifeCycle,
     RegularExp,
     REST,
