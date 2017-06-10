48af838ab870681366bafb45e29b98e2746b2373
niktekusho <nicola.dalmaso@outlook.com>
2017-06-10 19:09:20 +0200

    Versions major bump. Implemented frontend-side lifecycle communication

--- packages/monolith-frontend/src/Bubble/GenericBubble.test.js
+++ packages/monolith-frontend/src/Bubble/GenericBubble.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { createServer } from 'http';
+import serverIo from 'socket.io';
+import consts from 'monolith-consts';
 import GenericBubble from './GenericBubble';
 
 class TestGenericBubble extends GenericBubble {
@@ -34,5 +37,60 @@ describe('GenericBubble Test Suite', () => {
             component.update();
             expect(component.text()).toEqual('');
         });
+    });
+
+    describe('testing bubble\'s lifecycle', () => {
+        const originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        let app;
+        let serverSocket;
+        const port = 6543;
+
+        beforeEach(() => {
+            // 10 second timeout
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+            app = createServer();
+            app.listen(port);
+            serverSocket = serverIo(app).of(consts.BUBBLE_ROOM);
+        });
+
+        afterEach(() => {
+            app.close();
+
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+        });
+
+        it('should trigger lifecycled rendering', (done) => {
+            const url = `http://localhost:${port}`;
+            const component = shallow(<TestGenericBubble url={url} time={5} />);
+            serverSocket.on('connection', (clientSocket) => {
+                clientSocket.on(consts.LIFECYCLE_EVENT, (data) => {
+                    expect(data).toEqual(5);
+                    clientSocket.emit(consts.LIFECYCLE_EVENT_DONE);
+                    setTimeout(() => {
+                        expect(component.text()).toEqual('');
+                        done();
+                    }, 100);
+                });
+            });
+        });
+
+        it('should not trigger lifecycled rendering due to server error',
+            (done) => {
+                const url = `http://localhost:${port}`;
+                const component = shallow(
+                    <TestGenericBubble url={url} time={5} />
+                );
+                serverSocket.on('connection', (clientSocket) => {
+                    clientSocket.on(consts.LIFECYCLE_EVENT, (data) => {
+                        expect(data).toEqual(5);
+                        clientSocket.emit(consts.LIFECYCLE_EVENT_FAILED);
+                        setTimeout(() => {
+                            expect(component.text()).toEqual('test link');
+                            done();
+                        }, 100);
+                    });
+                });
+            }
+        );
     });
 });
