65fb6e5c61d4e25c0c2672f2c2c94f5e059d045e
niktekusho <nicola.dalmaso@outlook.com>
2017-06-10 14:59:39 +0200

    Added monolith-consts subproject. Server now manages bubbles' lifecycle

---
+++ packages/monolith-backend/src/Server/Bubble.test.js
@@ -0,0 +1,27 @@
+const io = require('socket.io-client');
+const commonConfig = require('./Server.config.test');
+const consts = require('monolith-consts');
+
+const url = commonConfig.url;
+
+const socket = io.connect(`${url}/${consts.BUBBLE_ROOM}`);
+
+const arg = process.argv.slice(2)[0];
+
+socket.once('connect', () => {
+    console.log('connected to server');
+    console.log(`emitting lifecycle with time ${arg}`);
+    socket.emit(consts.LIFECYCLE_EVENT, arg);
+
+    socket.once(consts.LIFECYCLE_EVENT_DONE, () => {
+        console.log('lifecycle-end received, emitting lifecycle-ok');
+        socket.emit(commonConfig.lifeCycleOk);
+        socket.disconnect();
+    });
+
+    socket.once(consts.LIFECYCLE_EVENT_FAILED, (error) => {
+        console.log(error);
+        socket.emit(commonConfig.lifeCycleOk);
+        socket.disconnect();
+    });
+});
