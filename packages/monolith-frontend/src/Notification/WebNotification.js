bce8bbc1e252fdd376b5c6aa9d645dcf5d4eaee6
niktekusho <nicola.dalmaso@outlook.com>
2017-06-15 23:38:48 +0200

    Enhanced Notifications behaviour. Update frontend project version

--- packages/monolith-frontend/src/Notification/WebNotification.js
+++ packages/monolith-frontend/src/Notification/WebNotification.js
@@ -5,14 +5,25 @@ export default class WebNotification {
         this.iconUrl = iconUrl;
     }
 
-    notify() {
-        if (Notification.permission !== 'granted') {
-            Notification.requestPermission();
-            return {};
-        }
+    createNotification() {
         return new Notification(
             this.title,
             { icon: this.iconUrl, body: this.body },
         );
+    }
+
+    notify() {
+        if (Notification.permission !== 'granted') {
+            Notification.requestPermission()
+                .then((permission) => {
+                    if (permission === 'granted') {
+                        return this.createNotification();
+                    }
+                    console.warn('Permission has not been granted.');
+                    return {};
+                })
+                .catch(error => console.error(error));
+        }
+        return this.createNotification();
     }
 }
